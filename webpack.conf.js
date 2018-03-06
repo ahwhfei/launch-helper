d55a49bb22ddb97e449a6d0131a53d8a9ebce4d6
Hongfei Wu <hongfei.wu@citrix.com>
2018-03-06 16:24:44 +0800

    add copy webpack plugin

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,13 +1,36 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    devServer: {
+        port: 8082,
+        historyApiFallback: true,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        }
+    },
     entry: './app/index.js',
     output: {
         filename: 'launch-helper.min.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new CopyWebpackPlugin([{
+                from: 'HDXEngine.html',
+                to: 'HDXEngine.html'
+            }, {
+                from: 'HDXLauncher.js',
+                to: 'HDXLauncher.js'
+            }, {
+                from: 'CitrixHTML5SDK.js',
+                to: 'CitrixHTML5SDK.js'
+            }, {
+                from: 'external',
+                to: 'external'
+            }
+        ])
     ]
 };
