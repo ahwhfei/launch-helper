d014a2fa67c16b115b32b75e588e6ebcc0f4cbe2
Hongfei Wu <hongfei.wu@citrix.com>
2018-04-04 17:57:36 +0800

    Update width and height

--- app/helper.js
+++ app/helper.js
@@ -33,8 +33,8 @@ class Helper {
             },
             bounds: {
                 autoresize: false,
-                width: '100%',
-                height: '100%'
+                width: HelperElement().clientWidth,
+                height: HelperElement().clientHeight
             },
             closeOptions: {
                 type: 'close',
